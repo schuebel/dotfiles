8fa88cc5fc2c4dd9d7ae4b69a7d0de02b98f9c46
Jonathan Schuebel (i565161) <jonathan.schuebel@sap.com>
2024-01-18 13:54:26 +0100

    added fish config

---
+++ fish/config.fish
@@ -0,0 +1,38 @@
+if status is-interactive
+  # enable vi mode
+  fish_vi_key_bindings
+
+  # vars
+  export FZF_DEFAULT_COMMAND='rg --files --hidden --ignore-file ~/.ignore'
+  # Aliases
+  alias vc='nvim -c "lua require(\'nvim-possession\').list()"'
+  alias lg=lazygit
+  alias ld=lazydocker
+  alias p=python3
+  alias vim='nvim'
+  alias sshus='ssh -o StrictHostKeyChecking=no'
+  alias ll="ls -l --color=auto"
+  alias la="ls -la --color=auto"
+  alias ls='ls --color=auto'
+  alias ..='cd ..'
+  alias wttr='curl wttr.in/st-leon-rot'
+  alias wttr2="curl -s 'wttr.in/st-leon-rot?format=v2'"
+  alias sampler='sampler --config ~/.config/sampler/config.yml'
+  alias g='git'
+
+  # FZF config
+  # excludes are handled via $HOME/.ignore
+  # Search for files with ctrl+f
+  fzf_configure_bindings --directory=\cf
+
+  # Accept next suggested word with CTRL-N
+  bind -M insert \cN forward-bigword
+  # Accept the entire suggestion with CTRL-U
+  bind -M insert \cU accept-autosuggestion
+
+  # Docker attach 
+  function dsh
+    docker container exec -it $argv[1] /bin/bash
+  end
+
+end
