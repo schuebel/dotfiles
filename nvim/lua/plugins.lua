dd88240b167f42a42feec9769df06875f17d7023
Jonathan Schuebel (i565161) <jonathan.schuebel@sap.com>
2024-01-27 00:12:18 +0100

    use telescope for project picker

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -330,54 +330,22 @@ require('lazy').setup({
             },
         },
     },
-    -- {
-    --     "schuebel/cd-project.nvim",
-    --     lazy = false,
-    --     -- keys = '<leader>sp',
-    --     -- config = function()
-    --     --     require("cd-project").setup({
-    --     --         -- this json file is acting like a database to update and read the projects in real time.
-    --     --         -- So because it's just a json file, you can edit directly to add more paths you want manually
-    --     --         projects_config_filepath = vim.fs.normalize(vim.fn.stdpath("config") .. "/cd-project.nvim.json"),
-    --     --         -- this controls the behaviour of `CdProjectAdd` command about how to get the project directory
-    --     --         project_dir_pattern = { ".git", ".gitignore", "Cargo.toml", "package.json", "go.mod" },
-    --     --         verbose = true,
-    --     --         hooks = {
-    --     --             {
-    --     --                 callback = function(dir)
-    --     --                     vim.notify("switched to dir: " .. dir)
-    --     --                 end,
-    --     --             },
-    --     --             {
-    --     --                 callback = function(dir)
-    --     --                     vim.notify("switched to dir: " .. dir)
-    --     --                 end,                         -- required, action when trigger the hook
-    --     --                 name = "cd hint",            -- optional
-    --     --                 order = 1,                   -- optional, the execution order if there're multiple hooks to be trigger at one point
-    --     --                 pattern = "cd-project.nvim", -- optional, trigger hook if contains pattern, optional
-    --     --                 trigger_point = "DISABLE",   -- optional, enum of trigger_points, default to `AFTER_CD`
-    --     --                 match_rule = function(dir)   -- optional, a function return bool. if have this fields, then pattern will be ignored
-    --     --                     return true
-    --     --                 end,
-    --     --             },
-    --     --         },
-    --     --     })
-    --     -- end,
-    -- }
-    -- Pick project (really just a directory switcher)
-    -- Telescope integration in progress: https://github.com/LintaoAmons/cd-project.nvim
+
     {
         "LintaoAmons/cd-project.nvim",
-        -- "schuebel/cd-project.nvim",
-        keys = '<leader>sp',
+        keys = {
+            { "<leader>sp", "<cmd>CdProject<cr>", desc = "[S]earch [P]rojects" },
+        },
         config = function()
-            vim.keymap.set('n', "<leader>sp", function() vim.cmd.CdProject() end)
-            require("cd-project").setup({ verbose = true })
-        end,
+            require("cd-project").setup({
+                projects_picker = "telescope"
+            })
+        end
     }
 }, { defaults = { lazy = true } })
 
-require("plugin-conf.telescope")
+-- load some more plugin specific stuff
 require("plugin-conf.treesitter")
 require("plugin-conf.lsp")
 require("plugin-conf.languages")
+require("plugin-conf.telescope")
