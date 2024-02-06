a5209df5951011fa2b5dd3ba0b159dd3a7bd4b6b
Jonathan Schuebel (i565161) <jonathan.schuebel@sap.com>
2024-02-06 15:35:41 +0100

    relpath in lualine

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -307,6 +307,14 @@ require('lazy').setup({
                 component_separators = '|',
                 section_separators = '',
             },
+            sections = {
+                lualine_c = {
+                    {
+                        'filename',
+                        path = 1, -- Relative path
+                    }
+                }
+            }
         },
     },
 
